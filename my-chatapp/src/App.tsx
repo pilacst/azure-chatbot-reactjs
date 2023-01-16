a62945319520673533e769e8866554f2f976f7c7
npilapitiya <npilapitiya@mitrai.com>
2023-01-16 11:35:31 +0530

    Initial commit

---
+++ my-chatapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ChatBot from './components/chatbot';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBot/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-chatapp/src/components/chatbot/index.tsx
@@ -0,0 +1,41 @@
+import { useMemo, useState, useEffect } from "react"
+import ReactWebChat, { createDirectLine } from "botframework-webchat"
+
+
+export const ChatBot = () => {
+  const [token, setToken] = useState("")
+  const [showChat, setShowChat] = useState(false)
+  const directLine = useMemo(() => createDirectLine({ token }), [token])
+
+//   useEffect(() => {
+//     API.post("/chatbot").then((data) => {
+//       setToken(data.data.token)
+//     })
+//   }, [showChat])
+
+const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+};
+
+const fetchData = async () => {
+    const response = await fetch("https://apim-01-downsizer-dev.azure-api.net/api/v1/chatbot", requestOptions)
+    const data = await response.json()
+    setToken(data.data.token)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
+  return (
+    <>
+      <div className="azure-chatbot">
+        {<ReactWebChat directLine={directLine} />}
+      </div>
+    </>
+  )
+}
+export default ChatBot
